735d97dee2de8b3dc6a6528facef79591a4fb376
Ilie Claudiu <claudiu.ilie0322@gmail.com>
2021-03-20 12:52:58 +0200

    add logs

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ require('dotenv').config()
 const createError = require('http-errors');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
 const authService = require('./services/authService');
-
+const loggerService = require('./services/loggerService')
 const indexRouter = require('./routes/main');
 const greenhouseSchedulerRouter = require('./routes/api/greenhouseSchedule');
 const greenhouseHistoryRouter = require('./routes/api/greenhouseHistory');
@@ -22,9 +21,8 @@ app.engine('hbs', exphbs({
 }));
 
 app.set('view engine', 'hbs');
+app.use(loggerService.logger);
 
-
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -44,9 +42,9 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
+
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.json({ message: err });
